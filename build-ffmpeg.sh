feab6736dbbae601a5a74c0cbc52454999282c2c
ilongge <1015820857@qq.com>
2021-12-08 21:25:59 +0800

    3.2.16

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -24,7 +24,7 @@ DEPLOYMENT_TARGET="8.0"
 # LAME=$(pwd)/extend/lame-ios
 
 # 编译FFmpeg版本
-FFMPEG_VERSION="3.1.11"
+FFMPEG_VERSION="3.2.16"
 
 if [[ $FFMPEG_VERSION != "" ]]; then
 	FFMPEG_VERSION=$FFMPEG_VERSION
